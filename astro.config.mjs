4129b00c55ff9ac6ae527493da9eea279b91903f
Marcio Toledo <toledox82@gmail.com>
2024-11-16 21:46:14 -0300

    Add: Install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marciotoledo.com',
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [preact()]
 });
